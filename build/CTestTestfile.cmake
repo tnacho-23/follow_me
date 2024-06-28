82eef98f172cf94bab973fa28f0a6d54d94ae1f2
tnacho-23 <ignacio.romero.a@ug.uchile.cl>
2024-06-28 12:27:05 -0400

    deppendencies fix

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bender/pioneer_ws/src/follow_me
+# Build directory: /home/bender/pioneer_ws/src/follow_me/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/bender/pioneer_ws/src/follow_me/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
